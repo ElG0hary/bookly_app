2f0e5423215cc4881f11b7799c2a33cbcde78652
ElG0hary <minidev99@gmail.com>
2024-05-14 00:21:34 +0300

    create font styles

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_bookly/core/utils/assets.dart';
 import 'package:my_bookly/core/utils/styles.dart';
 import 'package:my_bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:my_bookly/features/home/presentation/views/widgets/featured_list_view.dart';
@@ -18,7 +19,36 @@ class HomeViewBody extends StatelessWidget {
           SizedBox(height: 50),
           Text(
             'Best Sellers',
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
+          ),
+          SizedBox(height: 10),
+          BestSellersListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellersListViewItem extends StatelessWidget {
+  const BestSellersListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  image: AssetImage(AssetsData.testImage),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
           ),
         ],
       ),

d337ba415f2c4629e9a192414cc59351c1c2be1c
ElG0hary <minidev99@gmail.com>
2024-05-14 20:42:09 +0300

    best seller item part 2

---
+++ lib/features/home/presentation/views/widgets/best_seller_listview_item.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:my_bookly/constants.dart';
+import 'package:my_bookly/core/utils/assets.dart';
+import 'package:my_bookly/core/utils/styles.dart';
+
+class BestSellersListViewItem extends StatelessWidget {
+  const BestSellersListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  image: AssetImage(AssetsData.testImage),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 30),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: Text(
+                  "Harry Potter and the Goblet of Fire",
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: Styles.textStyle20
+                      .copyWith(fontFamily: kGtSectraFineFontFamily),
+                ),
+              ),
+              const SizedBox(height: 3),
+              Text(
+                "J.K Rowling",
+                overflow: TextOverflow.ellipsis,
+                style: Styles.textStyle14
+                    .copyWith(color: Colors.white.withOpacity(.7)),
+              ),
+              const SizedBox(height: 3),
+              Row(
+                children: [
+                  Text(
+                    '19.99 \$',
+                    style: Styles.textStyle20
+                        .copyWith(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:my_bookly/constants.dart';
 import 'package:my_bookly/core/utils/assets.dart';
 import 'package:my_bookly/core/utils/styles.dart';
+import 'package:my_bookly/features/home/presentation/views/widgets/best_seller_listview_item.dart';
 import 'package:my_bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:my_bookly/features/home/presentation/views/widgets/featured_list_view.dart';
 
@@ -24,47 +25,6 @@ class HomeViewBody extends StatelessWidget {
           ),
           SizedBox(height: 20),
           BestSellersListViewItem(),
-        ],
-      ),
-    );
-  }
-}
-
-class BestSellersListViewItem extends StatelessWidget {
-  const BestSellersListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                image: const DecorationImage(
-                  image: AssetImage(AssetsData.testImage),
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(width: 30),
-          Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: const Text(
-                  "Harry Potter and the Goblet of Fire",
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
-                  style: Styles.textStyle20,
-                ),
-              ),
-            ],
-          )
         ],
       ),
     );

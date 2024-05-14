c3b0db21246dba1cd017189323fce78786171445
ElG0hary <minidev99@gmail.com>
2024-05-14 22:39:58 +0300

    create custom book details AppBar

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,11 @@
 import 'package:go_router/go_router.dart';
+import 'package:my_bookly/features/home/presentation/views/book_details_view.dart';
 import 'package:my_bookly/features/home/presentation/views/home_view.dart';
 import 'package:my_bookly/features/splash/presentation/views/splash_view.dart';
 
 abstract class AppRouter {
   static const kHomeViewRoute = '/homeView';
+  static const kBookDetailsViewRoute = '/bookDetailsView';
   static final router = GoRouter(routes: [
     GoRoute(
       path: '/',
@@ -12,6 +14,10 @@ abstract class AppRouter {
     GoRoute(
       path: kHomeViewRoute,
       builder: (context, state) => const HomeView(),
+    ),
+    GoRoute(
+      path: kBookDetailsViewRoute,
+      builder: (context, state) => const BookDetailsView(),
     ),
   ]);
 }
--- lib/features/home/presentation/views/widgets/best_seller_listview_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_listview_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:my_bookly/constants.dart';
+import 'package:my_bookly/core/utils/app_router.dart';
 import 'package:my_bookly/core/utils/assets.dart';
 import 'package:my_bookly/core/utils/styles.dart';
 import 'package:my_bookly/features/home/presentation/views/widgets/book_rating.dart';
@@ -9,59 +11,62 @@ class BestSellersListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                image: const DecorationImage(
-                  image: AssetImage(AssetsData.testImage),
-                  fit: BoxFit.fill,
+    return GestureDetector(
+      onTap: () => GoRouter.of(context).push(AppRouter.kBookDetailsViewRoute),
+      child: SizedBox(
+        height: 125,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.5 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  image: const DecorationImage(
+                    image: AssetImage(AssetsData.testImage),
+                    fit: BoxFit.fill,
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(width: 30),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * .5,
-                  child: Text(
-                    "Harry Potter and the Goblet of Fire",
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                    style: Styles.textStyle20
-                        .copyWith(fontFamily: kGtSectraFineFontFamily),
-                  ),
-                ),
-                const SizedBox(height: 3),
-                Text(
-                  "J.K Rowling",
-                  overflow: TextOverflow.ellipsis,
-                  style: Styles.textStyle14
-                      .copyWith(color: Colors.white.withOpacity(.7)),
-                ),
-                Row(
-                  children: [
-                    Text(
-                      '19.99 \$',
+            const SizedBox(width: 30),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * .5,
+                    child: Text(
+                      "Harry Potter and the Goblet of Fire",
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
                       style: Styles.textStyle20
-                          .copyWith(fontWeight: FontWeight.w800),
+                          .copyWith(fontFamily: kGtSectraFineFontFamily),
                     ),
-                    const Spacer(),
-                    const BookRating(),
-                  ],
-                ),
-              ],
-            ),
-          )
-        ],
+                  ),
+                  const SizedBox(height: 3),
+                  Text(
+                    "J.K Rowling",
+                    overflow: TextOverflow.ellipsis,
+                    style: Styles.textStyle14
+                        .copyWith(color: Colors.white.withOpacity(.7)),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        '19.99 \$',
+                        style: Styles.textStyle20
+                            .copyWith(fontWeight: FontWeight.w800),
+                      ),
+                      const Spacer(),
+                      const BookRating(),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }

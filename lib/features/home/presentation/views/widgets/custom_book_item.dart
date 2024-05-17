64a34f8f092d15dcc79f897f3733c6883af97000
ElG0hary <minidev99@gmail.com>
2024-05-17 14:16:16 +0300

    add custom book item to Book Details View

--- lib/features/home/presentation/views/widgets/custom_book_item.dart
+++ lib/features/home/presentation/views/widgets/custom_book_item.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:my_bookly/core/utils/assets.dart';
 
-class CustomFeaturedListViewItem extends StatelessWidget {
-  const CustomFeaturedListViewItem({super.key});
+class CustomBookItem extends StatelessWidget {
+  const CustomBookItem({super.key});
 
   @override
   Widget build(BuildContext context) {
